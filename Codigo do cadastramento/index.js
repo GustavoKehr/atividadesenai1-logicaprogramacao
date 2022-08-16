1140b71ab193e14ea02b2ae029ef426cbe1893b9
GustavoKehr <guckehr008@gmail.com>
2022-08-15 22:57:16 -0300

    Novas linhas e aprimoramentos

--- Codigo do cadastramento/index.js
+++ Codigo do cadastramento/index.js
@@ -1,33 +1,39 @@
-const data = [10];
-let participantes = [95];
-let datadecadastro = [9];
-
-const idade = [18];
-let idadedoparticipante = [18];
+let dataAtual = new Date (2022,8,14);
+let dataEvento = new Date(2022,11,12);
+let participantes = ["Gustavo", "Carlos", "Adriano", "Cintia", "Gabriela"]
+let idadedoparticipante = 18;
 
 console.log("----------------------------------------------------------------------------------")
 console.log("------------------------------Inicio do cadastramento-----------------------------")
 console.log("----------------------------------------------------------------------------------")
 
-if (data => 10) { console.log("Prosseguir cadastramento para o evento"); }
-else if (datadecadastro < 10) { console.log("Cadastremento não autorizado devido a data invalida"); }
+if (dataEvento > dataAtual) { 
+    console.log("Prosseguir cadastramento para o evento");
+ } else { 
+    console.log("Cadastremento não autorizado devido a data invalida"); }
 
 console.log("----------------------------------------------------------------------------------")
 
-if (idadedoparticipante => 18) { console.log("Prosseguir cadastramento para o evento"); }
-else if (idadedoparticipante < 18) { console.log ("Idade inválida, não foi possível prosseguir com seu cadastramento"); }
+if (idadedoparticipante >= 18) { 
+    console.log("Prosseguir cadastramento para o evento");
+} else { 
+    console.log ("Idade inválida, não foi possível prosseguir com seu cadastramento"); }
 
 console.log("----------------------------------------------------------------------------------")
 
-if (participantes < 100) { console.log("Prosseguir cadastramento para o evento"); }
-else if (participantes > 100) { console.log("Cadastramento não autorizado devido ao excesso de cadastros"); }
+if (participantes.length < 100) { 
+    console.log(participantes)
+    console.log("Prosseguir cadastramento para o evento");
+} else { 
+    console.log("Cadastramento não autorizado devido ao excesso de cadastros");
+ }
 
 console.log("----------------------------------------------------------------------------------")
 
-if (participantes < 100, data > datadecadastro, participantes < 100); {
+if (participantes < 100, dataEvento > dataAtual, participantes < 100); {
     console.log("O cadastro pode ser realizado")
     participantes++
-    console.log(participantes)
+    console.log(participantes.length)
 }
 
 console.log("-------------------------------Fim do Cadastramento-------------------------------")
